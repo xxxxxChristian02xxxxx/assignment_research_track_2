07d8bc40eb1234c0ba3000d945e26ddfaf3cb077
Christian <negcris02@gmail.com>
2024-12-24 17:02:10 +0000

    adding the message costom and the pubisher of the velocity and positivion

--- assignment_rt2_ex1/src/client_action.cpp
+++ assignment_rt2_ex1/src/client_action.cpp
@@ -4,20 +4,35 @@
 #include <actionlib/client/terminal_state.h>
 // librerie del file action contenuto nel pacchetto
 #include <assignment_2_2024/PlanningAction.h>
+// include of the message custom 
+#include <assignment_rt2_ex1/M_vel_pos.h>
+#include <nav_msgs/Odometry.h>
 // useful libraries
 #include <iostream>
 #include <string>
 #include <std_msgs/String.h>
  
+ // creation of the custom message
+assignment_rt2_ex1::M_vel_pos vel_pose;
+ 
+ 
+void messageCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    vel_pose.x = msg->pose.pose.position.x;
+    vel_pose.y = msg->pose.pose.position.y;
+    vel_pose.vel_x = msg->twist.twist.linear.x;
+    vel_pose.vel_z = msg->twist.twist.angular.z;
+}
  
 int main (int argc, char** argv){
 	ros::init(argc, argv, "client_action_target");
 	ros::NodeHandle nh;
 	
 	// Publisher to publish the velocity and position
-	//ros::Publisher pub_pos_vel = nh.advertise<std_msg::String>("",10);
-	
+	ros::Publisher pub_pos_vel = nh.advertise<assignment_rt2_ex1::M_vel_pos>("new_topic",10); // change name 
 
+	// subscriber on odom
+	ros::Subscriber sub = nh.subscribe("odom", 1000, messageCallback);
+	
 	// creation of the instance of the action service 
 	actionlib::SimpleActionClient<assignment_2_2024::PlanningAction> client("/reaching_goal",true);
 	// calling the server and waitng for it starts
@@ -44,6 +59,8 @@ int main (int argc, char** argv){
 	
  	while(ros::ok() && !moving_toggle){
  		
+ 		pub_pos_vel.publish(vel_pose);
+ 		
  		// following the composition of the file.action
  		// goal definition: geometry_msgs/PoseStamped target_pose
  		// result definition: N/A
@@ -64,13 +81,6 @@ int main (int argc, char** argv){
  		
  		// sending the goal 
  		client.sendGoal(goal);
- 		
- 		
- 		
- 		
-
- 		
- 		
  		
  		ros::spinOnce();
  	
