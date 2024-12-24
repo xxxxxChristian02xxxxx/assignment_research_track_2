d74517900c341adfee3bc1ff752411478dafcacf
Christian <negcris02@gmail.com>
2024-12-24 23:19:48 +0000

    added the parameters to communicate between the service node and the acion client, adjusted the functionality of asking after some time the user to know if he wants te robot to continue reaching the last goal or to change it

--- assignment_rt2_ex1/src/client_action.cpp
+++ assignment_rt2_ex1/src/client_action.cpp
@@ -12,9 +12,14 @@
 #include <string>
 #include <std_msgs/String.h>
  
- // creation of the custom message
+// creation of the custom message
 assignment_rt2_ex1::M_vel_pos vel_pose;
- 
+int moving_toggle = true;
+
+// creation of the instance of the action service 
+actionlib::SimpleActionClient<assignment_2_2024::PlanningAction> client("/reaching_goal", true);
+
+
  
 void messageCallback(const nav_msgs::Odometry::ConstPtr& msg) {
     vel_pose.x = msg->pose.pose.position.x;
@@ -22,44 +27,51 @@ void messageCallback(const nav_msgs::Odometry::ConstPtr& msg) {
     vel_pose.vel_x = msg->twist.twist.linear.x;
     vel_pose.vel_z = msg->twist.twist.angular.z;
 }
+
+void callback_timer(const ros::TimerEvent&){
+ 	moving_toggle= false;
+	// retrieving the feedback from the service 
+	assignment_2_2024::PlanningActionFeedback::ConstPtr fdk = ros::topic::waitForMessage<assignment_2_2024::PlanningActionFeedback>("/reaching_goal/feedback");
+	
+	// listening to the feedback and seeing if the robot is moving, if yes, the user can choose if to stop it or to let it move
+	if ((fdk->feedback.stat !="Target reached!")&& (fdk->feedback.stat != "Target cancelled")){
+ 		int user_block_decision=0;
+ 		std::cout<<"The robot is moving \n1 -- cancel the taget \n2 -- keep on moving "<< std::endl;	
+ 		std::cin >> user_block_decision;
+ 		if (user_block_decision){
+ 			client.cancelGoal();
+
+ 		}
+	}
+	moving_toggle= true;
+}
+
  
 int main (int argc, char** argv){
 	ros::init(argc, argv, "client_action_target");
 	ros::NodeHandle nh;
+	
+	
+
+	// calling the server and waitng for it starts
+	client.waitForServer();
 	
 	// Publisher to publish the velocity and position
 	ros::Publisher pub_pos_vel = nh.advertise<assignment_rt2_ex1::M_vel_pos>("new_topic",10); // change name 
 
 	// subscriber on odom
 	ros::Subscriber sub = nh.subscribe("odom", 1000, messageCallback);
-	
-	// creation of the instance of the action service 
-	actionlib::SimpleActionClient<assignment_2_2024::PlanningAction> client("/reaching_goal",true);
-	// calling the server and waitng for it starts
-	client.waitForServer();
 
-	// retrieving the feedback from the service 
-	assignment_2_2024::PlanningActionFeedback::ConstPtr fdk = ros::topic::waitForMessage<assignment_2_2024::PlanningActionFeedback>("/reaching_goal/feedback");
+	// timer which each 5 seconds asks the user to keep moving or cancel target
+	ros::Timer timer = nh.createTimer(ros::Duration(5),callback_timer);
 	
-	 // listening to the feedback and seeing if the robot is moving, if yes, the user can choose if to stop it or to let it move
-	int moving_toggle = true;
-	if ((fdk->feedback.stat !="Target reached!")&& (fdk->feedback.stat != "Target cancelled")){
- 			int user_block_decision=0;
- 			std::cout<<"The robot is moving \n 1 -- cancel the taget \n 2 -- keep on moving "<< std::endl;	
- 			std::cin >> user_block_decision;
- 			
- 			if (user_block_decision){
- 				client.cancelGoal();
- 				moving_toggle= false;
- 			}
-	}
-	
-	
-	
-	
- 	while(ros::ok() && !moving_toggle){
- 		
+ 	while(ros::ok()){
+		if (!moving_toggle){
+			continue;
+		}
+		
  		pub_pos_vel.publish(vel_pose);
+ 		
  		
  		// following the composition of the file.action
  		// goal definition: geometry_msgs/PoseStamped target_pose
@@ -67,31 +79,32 @@ int main (int argc, char** argv){
  		// feedback definition: geometry_msgs/Pose actual_pose, string stat
  
  		// if the robot is not moving, the ser will be asked to set the target position 
- 		double target_pos_x,target_pos_y, orientation;
+ 		float target_pos_x,target_pos_y, target_ore_z;
  		std::cout << "Enter the target coordinates:" << std::endl; 
  		std::cout << "X:"; std::cin >> target_pos_x;
   		std::cout << "Y:"; std::cin >> target_pos_y;
-  		std::cout << "Orientation:"; std::cin >> orientation;
+  		std::cout << "Orientation:"; std::cin >> target_ore_z;
  		
  		// building the goal 
  		assignment_2_2024::PlanningGoal goal;
  		goal.target_pose.pose.position.x = target_pos_x;
  		goal.target_pose.pose.position.y = target_pos_y;
- 		goal.target_pose.pose.orientation.z = orientation;
+ 		goal.target_pose.pose.orientation.z = target_ore_z;
  		
- 		// sending the goal 
+		// memorizing the parameters int he server 
+       	nh.setParam("/last_target/x", target_pos_x);
+        	nh.setParam("/last_target/y", target_pos_y);
+        	nh.setParam("/last_target/orientation", target_ore_z);
+ 		
+ 		// sending the goal	
  		client.sendGoal(goal);
+ 		
+ 		
  		
  		ros::spinOnce();
  	
  	}
  	
-
-	
-	
-	
-	
-	
 	return 0;
 }
 
---
+++ assignment_rt2_ex1/src/last_target_coordinates_service.cpp
@@ -0,0 +1,28 @@
+include "ros/ros.h"
+#include "assignment_rt2_ex1/Service_target_coordinates.h" 
+#include <stdlib.h>
+#include <stdio.h>
+
+bool show_last_target_cooridinates(assignment_rt2_ex1::Service_target_coordinates::Response &res){
+	ros::NodeHandle nh;
+	// checking if the parameters are present
+	if (nh.hasParam("/last_target/x") && nh.hasParam("/last_target/y") && nh.hasParam("/last_target/orientation")){
+		// retreivng the parameters and giving thema as response
+		nh.getParam("/last_target/x", res.target_pos_x) ;
+		nh.getParam("/last_target/y", res.target_pos_y) ;
+		nh.getParam("/last_target/orientation", res.target_orie_z); 
+		return true;
+	} else {
+      		return false;
+   	}
+}
+
+ int main(int argc, char **argv)
+ {
+ 	ros::init(argc, argv, "last_target_coordinates_sent_server"); 
+ 	ros::NodeHandle n;
+ 	ros::ServiceServer service = n.advertiseService("/last_target_coordinates_sent_server",show_last_target_cooridinates);
+	ros::spin();
+ 	return 0;
+
+ }
