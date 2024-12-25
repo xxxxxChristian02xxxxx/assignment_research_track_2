7de1d7cf999971f667e02b547e58d51c04c3052d
Christian <negcris02@gmail.com>
2024-12-25 00:14:42 +0000

    launch file

--- assignment_rt2_ex1/src/last_target_coordinates_service.cpp
+++ assignment_rt2_ex1/src/last_target_coordinates_service.cpp
@@ -1,16 +1,16 @@
-include "ros/ros.h"
+#include "ros/ros.h"
 #include "assignment_rt2_ex1/Service_target_coordinates.h" 
 #include <stdlib.h>
 #include <stdio.h>
 
-bool show_last_target_cooridinates(assignment_rt2_ex1::Service_target_coordinates::Response &res){
+bool show_last_target_cooridinates(assignment_rt2_ex1::Service_target_coordinates::Request &req,assignment_rt2_ex1::Service_target_coordinates::Response &res){
 	ros::NodeHandle nh;
 	// checking if the parameters are present
 	if (nh.hasParam("/last_target/x") && nh.hasParam("/last_target/y") && nh.hasParam("/last_target/orientation")){
 		// retreivng the parameters and giving thema as response
 		nh.getParam("/last_target/x", res.target_pos_x) ;
 		nh.getParam("/last_target/y", res.target_pos_y) ;
-		nh.getParam("/last_target/orientation", res.target_orie_z); 
+		nh.getParam("/last_target/orientation", res.target_orie_z);
 		return true;
 	} else {
       		return false;
