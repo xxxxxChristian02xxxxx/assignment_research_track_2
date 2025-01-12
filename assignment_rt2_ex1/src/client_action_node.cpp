19aedd57749b92165e41f9980eb641c85a012dc8
Christian <negcris02@gmail.com>
2025-01-12 13:32:41 +0000

    esercizio su aggiungere iu target e chiamare il server

--- assignment_rt2_ex1/src/client_action_node.cpp
+++ assignment_rt2_ex1/src/client_action_node.cpp
@@ -11,11 +11,30 @@
 #include <iostream>
 #include <string>
 #include <std_msgs/String.h>
- 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <vector> 
+#include "assignment_rt2_ex1/Service_target_coordinates.h"
+ #include <std_msgs/Float32.h>
+ #include <sensor_msgs/LaserScan.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 // creation of the custom message
 assignment_rt2_ex1::M_vel_pos vel_pose;
 ros::Publisher pub_pos_vel;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+ros::Publisher publish_distance;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 assignment_2_2024::PlanningActionFeedback::ConstPtr fdk;
+int dim = 3;
+std::vector<nav_msgs::Odometry> target_array;
+int num_elements =0;
+
 
 // 
 void messageCallback(const nav_msgs::Odometry::ConstPtr& msg) {
@@ -40,8 +59,20 @@ void decide_action_in_moving( actionlib::SimpleActionClient<assignment_2_2024::P
 	 	ros::Duration(1).sleep();
 	 } 		
 }
-
-
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    float min_distance = std::numeric_limits<float>::infinity();
+    for (const auto& range : msg->ranges) {
+        if (range < min_distance) {
+            min_distance = range;
+        }
+    }
+    std_msgs::Float32 closest_obstacle_msg;
+    closest_obstacle_msg.data = min_distance;
+    publish_distance.publish(closest_obstacle_msg);
+}
+void feedbackCallback(const assignment_2_2024::PlanningActionFeedback::ConstPtr& msg) {
+    fdk = msg;
+}
 assignment_2_2024::PlanningGoal set_target(){
 	   	ros::NodeHandle nh;
 
@@ -52,9 +83,7 @@ assignment_2_2024::PlanningGoal set_target(){
  		
  		// if the robot is not moving, the ser will be asked to set the target position 
  		float target_pos_x,target_pos_y, target_ore_z;
- 		std::cout << "Enter the target coordinates:" << std::endl; 
- 		std::cout << "X:"; std::cin >> target_pos_x;
-  		std::cout << "Y:"; std::cin >> target_pos_y;
+
  		
  		// building the goal 
  		assignment_2_2024::PlanningGoal goal;
@@ -65,20 +94,50 @@ assignment_2_2024::PlanningGoal set_target(){
  		
         	return goal;
 }
-void feedbackCallback(const assignment_2_2024::PlanningActionFeedback::ConstPtr& msg) {
-    fdk = msg;
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+assignment_2_2024::PlanningGoal set_target2(nav_msgs::Odometry target_element){
+	   	ros::NodeHandle nh;
 
- 
+	 	// following the composition of the file.action
+ 		// goal definition: geometry_msgs/PoseStamped target_pose
+ 		// result definition: N/A
+ 		// feedback definition: geometry_msgs/Pose actual_pose, string stat
+ 		
+ 		// if the robot is not moving, the ser will be asked to set the target position 
+ 		//float target_pos_x,target_pos_y, target_ore_z;
+
+ 		
+ 		// building the goal 
+ 		assignment_2_2024::PlanningGoal goal;
+ 		goal.target_pose.pose.position.x = target_element.pose.pose.position.x;
+ 		goal.target_pose.pose.position.y = target_element.pose.pose.position.x;
+
+
+ 		
+        return goal;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void call_service(){
+	ros::NodeHandle nh;
+	ros::ServiceClient client = nh.serviceClient<assignment_rt2_ex1::Service_target_coordinates>("Service_target_coordinates");
+	assignment_rt2_ex1::Service_target_coordinates srv;
+	ROS_INFO("The target coordinates are: %f %f", srv.response.target_pos_x, srv.response.target_pos_y);
+
+}
+ ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int main (int argc, char** argv){
 	// Inizializza ROS
     	ros::init(argc, argv, "client_action_target");
+
 
    	// Crea il NodeHandle dopo ros::init()
    	ros::NodeHandle nh;
 	// creation of the instance of the action service 
 	actionlib::SimpleActionClient<assignment_2_2024::PlanningAction> client("/reaching_goal", true);
-	
+
 
 	// calling the server and waitng for it starts
 	client.waitForServer();
@@ -89,20 +148,83 @@ int main (int argc, char** argv){
 	// subscriber on odom
 	ros::Subscriber sub = nh.subscribe("odom", 1000, messageCallback);
 	ros::Subscriber feedback_sub = nh.subscribe("/reaching_goal/feedback", 10, feedbackCallback);
+	ros::Subscriber laser_sub = nh.subscribe("scan", 1000, laserCallback);
+
+	float target_pos_x,target_pos_y, target_ore_z;
+
+	//assignment_2_2024::PlanningGoal goal = set_target();
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	int j =0;
+	while(true){
+		std::cout << "Enter the target coordinates:" << std::endl; 
+		std::cout << "X:"; std::cin >> target_pos_x;
+		std::cout << "Y:"; std::cin >> target_pos_y;
+		target_array[j].pose.pose.position.x =target_pos_x ;
+		target_array[j].pose.pose.position.y =target_pos_y;
+		std::cout << "Do you want to add another target? (y/n)" << std::endl;
+		char cont;
+		std::cin >> cont;
+		if(cont == 'n'){
+			break;
+		} 
+		j++;
+	}
+	assignment_2_2024::PlanningGoal goal = set_target2(target_array[0]);
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	client.sendGoal(goal);
 
 
-	assignment_2_2024::PlanningGoal goal = set_target();
-        client.sendGoal(goal);
  	while(ros::ok()){
  		pub_pos_vel.publish(vel_pose);
  		ros::spinOnce();
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		std::cout << "do you want to call the service? (y/n)" << std::endl;
+		char cont;
+		std::cin >> cont;
+		if(cont == 'y'){
+			call_service();
+		}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 	 	if (fdk != nullptr) {
 
 			// making the goal if it is not moving
 		   	 if (fdk->feedback.stat == "Target reached!" ||fdk->feedback.stat == "Target cancelled!" ) {
-				goal = set_target();
-				client.sendGoal(goal);	
+
+				// if i have reached all the targets then i will clear the vector				
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+				if(num_elements == target_array.size()){
+					num_elements =0;	
+					target_array.clear();			
+				}
+				// if vector is empty then fill it in
+				if(target_array.empty()){
+					char cont;
+					int i =0;
+					while(true){
+						std::cout << "Enter the target coordinates:" << std::endl; 
+						std::cout << "X:"; std::cin >> target_pos_x;
+						std::cout << "Y:"; std::cin >> target_pos_y;
+						target_array[i].pose.pose.position.x =target_pos_x ;
+						target_array[i].pose.pose.position.y =target_pos_y;
+						std::cout << "Do you want to add another target? (y/n)" << std::endl;
+						std::cin >> cont;
+						if(cont == 'n'){
+							break;
+						} 
+						i++;
+					}
+					goal = set_target2(target_array[num_elements]);
+				}else{
+					// otherwise keep on sending the target
+					goal = set_target2(target_array[num_elements]);
+				}
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+				//goal = set_target();
+				client.sendGoal(goal);
+				num_elements++;	
 
 				
 			} else if (fdk->feedback.stat == "State 0: go to point" || fdk->feedback.stat == "State 1: avoid obstacle") { // giving possibility to start again if moving
