7a184f3b2f8e40e2d5aae95fe9c8bf2a32dfb96b
Christian <negcris02@gmail.com>
2025-03-22 11:15:19 +0000

    fies

---
+++ docs/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['call_5fservice_17',['call_service',['../client__action__node_8cpp.html#ab905db6342f58bdf6be67f7087e95b62',1,'client_action_node.cpp']]]
+];
---
+++ docs/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lasercallback_20',['laserCallback',['../client__action__node_8cpp.html#a39fa80704c3dd9378ba49ed5009bab78',1,'client_action_node.cpp']]]
+];
