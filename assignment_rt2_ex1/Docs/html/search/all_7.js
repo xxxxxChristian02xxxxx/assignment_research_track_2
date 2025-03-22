674ae885f141c00212fada1a37af84325d282ecf
Christian <negcris02@gmail.com>
2025-03-22 11:31:59 +0000

    clear

--- assignment_rt2_ex1/Docs/html/search/all_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['set_5ftarget_12',['set_target',['../client__action__node_8cpp.html#a99b87c623ea411bd683be10ef1dada7e',1,'client_action_node.cpp']]],
-  ['show_5flast_5ftarget_5fcooridinates_13',['show_last_target_cooridinates',['../last__target__coordinates__service_8cpp.html#a745d25ca656c01c68d3e84f7a99b087f',1,'last_target_coordinates_service.cpp']]]
-];
--- assignment_rt2_ex1/Docs/html/search/variables_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['target_5farray_29',['target_array',['../client__action__node_8cpp.html#a6606627ae2b60ad19d8819c46eedde0b',1,'client_action_node.cpp']]]
-];
